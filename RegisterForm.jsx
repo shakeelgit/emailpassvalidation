dd0a49e9178a55c09e9f1760258173240c2f1bc0
shakeelgit <shakeel5282@gmail.com>
2020-08-02 19:14:12 +0530

    Add files via upload

---
+++ RegisterForm.jsx
@@ -0,0 +1,119 @@
+import React from 'react';
+import './Style.css';
+
+
+class RegisterForm extends React.Component {
+ 
+
+    constructor() {
+      super();
+      this.state = {
+        fields: {},
+        errors: {}
+      }
+
+      this.handleChange = this.handleChange.bind(this);
+      this.submituserRegistrationForm = this.submituserRegistrationForm.bind(this);
+
+    };
+
+    handleChange(e) {
+      let fields = this.state.fields;
+      fields[e.target.name] = e.target.value;
+      this.setState({
+        fields
+      });
+
+    }
+
+    submituserRegistrationForm(e) {
+      e.preventDefault();
+      if (this.validateForm()) {
+          let fields = {};
+          
+          fields["emailid"] = "";
+          
+          fields["password"] = "";
+          this.setState({fields:fields});   
+          alert(`Hello ${this.state.fields.emailid.substring(0, this.state.fields.emailid.indexOf("@"))}`);
+      }
+
+    }
+
+    validateForm() {
+
+      let fields = this.state.fields;
+      let errors = {};
+      let formIsValid = true;
+
+        if (!fields["emailid"]) {
+        formIsValid = false;
+        errors["emailid"] = "*Please enter your email-ID.";
+      }
+
+      if (typeof fields["emailid"] !== "undefined") {
+        //regular expression for email validation
+        var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+        if (!pattern.test(fields["emailid"])) {
+          formIsValid = false;
+          errors["emailid"] = "*Please enter valid email-ID.";
+        }
+      }
+
+     
+      if (!fields["password"]) {
+        formIsValid = false;
+        errors["password"] = "*Please enter your password.";
+      }
+
+      if (typeof fields["password"] !== "undefined") {
+        if (!fields["password"].match(/^.*(?=.*[A-Z])/)) {
+          formIsValid = false;
+          errors["password"] = "*Password should contain one uppercase letter   ";
+        }
+      }
+
+      this.setState({
+        errors: errors
+      });
+      return formIsValid;
+
+
+    }
+
+
+
+  render() {
+    return (
+    <div id="main-registration-container">
+     <div id="register">
+        <h3>Login Form</h3>
+        <form method="post"  name="userRegistrationForm"  onSubmit= {this.submituserRegistrationForm} >
+       
+        
+        <label>Email ID:</label>
+        <input type="text" name="emailid" value={this.state.fields.emailid} onChange={this.handleChange}  />
+        <div className="errorMsg">{this.state.errors.emailid}
+        
+       
+        
+        </div>
+        
+        
+        <label>Password:</label>
+        <input type="password" name="password" value={this.state.fields.password} onChange={this.handleChange} />
+        <div className="errorMsg">{this.state.errors.password}</div>
+        <input type="submit" className="button"  value="Login"/>
+        
+        </form>
+    </div>
+</div>
+
+      );
+  }
+
+
+}
+
+
+export default RegisterForm;
---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import registerServiceWorker from './registerServiceWorker';
+import RegisterForm from './RegisterForm';
+import {BrowserRouter} from "react-router-dom"; 
+
+ReactDOM.render(
+<BrowserRouter><RegisterForm /></BrowserRouter>,
+
+
+document.getElementById('root'));
+
+//registerServiceWorker();  
